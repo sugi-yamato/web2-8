cf6655666c04b86b98a5535548525da9a693f8d9
sugi-yamato <38421455+sugi-yamato@users.noreply.github.com>
2018-11-19 17:01:43 +0900

    Create default.js

---
+++ default.js
@@ -0,0 +1,17 @@
+var separate_time = function(time){
+    var sec = time.getSeconds();
+    var min = time.getMinutes();
+    var hours = time.getHours();
+    var month = time.getMonth();
+    var year = time.getHullYear();
+    return[sec,min,hours,days,month,year];
+}
+
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById('countdown').textContent =
+counter[5] + '年' +
+counter[4] + '日' +
+counter[3] + '時' +
+counter[2] + '分' +
+counter[1] + '秒'；
